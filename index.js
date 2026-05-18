b9183110b8e38e03b1d31f3e37dbfda3d6769e7c
Farhan Sadik <farhansadik703@gmail.com>
2026-05-19 01:18:16 +0600

    mongodb connection completed

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 /////////////////////////////////////
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+
+const uri = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("drivefleet");
+    const carCollection = db.collection("cars");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } catch (error) {
+    console.error("MongoDB connection faild:", error);
+    process.exit(1);
+  }
+}
+run().catch(console.dir);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
